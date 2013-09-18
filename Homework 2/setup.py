efb0ddbea979f75b0e699b989a4d6f32ac8566f5
Anders Priest <anders.priest@gmail.com>
2013-09-18 16:47:26 -0700

    Added functionality to CalCalc.py and added setup.py

---
+++ Homework 2/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'CalCalc',
+        version = '1.0',
+        author = 'Anders Priest',
+        author_email = 'anders.priest@gmail.com',
+        py_modules = ['CalCalc'],
+        )
